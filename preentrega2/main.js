684cda38973110d29b712dc5e22ea613a15e60ae
jachaval <slash.glp@gmail.com>
2022-07-09 20:27:07 -0300

    2dapre

---
+++ preentrega2/main.js
@@ -0,0 +1,24 @@
+const productos = [
+    {id:'0', tipo: "entrenamiento", modelo: "mancuerna", precio: 11.99, img: "" },
+    {id:'1', tipo: "entrenamiento", modelo: "discos", precio: 29.99, img: "" },
+    {id:'2', tipo: "entrenamiento", modelo: "soga", precio: 4.99, img: "" },
+    {id:'3', tipo: "entrenamiento", modelo: "ketbell", precio: 19.99, img: "" },
+    {id:'4', tipo: "entrenamiento", modelo: "barracompleta'", precio: 45.00, img: "g" },
+    {id:'5', tipo: "entrenamiento", modelo: "banda'", precio: 9.99, img: "" },
+    {id:'6', tipo: "entrenamiento", modelo: "rodillo", precio: 5.99, img: "" },
+    {id:'7', tipo: "entrenamiento", modelo: "rueda", precio: 5.99, img: "" },
+    {id:'8', tipo: "entrenamiento", modelo: "tobilleras", precio: 5.99, img: "" },
+    {id:'9', tipo: "entrenamiento", modelo: "reloj", precio: 18.99, img: "" },
+    {id:'10', tipo: "nutricion", modelo: "proteina1", precio: 19.99, img: "" },
+    {id:'11', tipo: "nutricion", modelo: "proteina2", precio: 19.99, img: "" },
+    {id:'12', tipo: "nutricion", modelo: "proteina3", precio: 19.99, img: "" },
+    {id:'13', tipo: "nutricion", modelo: "amino1", precio: 15.99, img: "" },
+    {id:'14', tipo: "nutricion", modelo: "amino2", precio: 15.99, img: "" },
+
+  ];
+
+  const contenedorTienda = document.getElementById('container-lg');
+ 
+
+  
+
